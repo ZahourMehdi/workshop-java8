9f67828b73a9ff565add12b3987ff13c9214a05a
Mehdi.Zahour <zahour.mehdi@gmail.com>
2018-03-29 12:29:20 +0200

    TP java 8 (04 optional)

--- src/test/java/java8/ex03/Optional_03_Test.java
+++ src/test/java/java8/ex03/Optional_03_Test.java
@@ -45,7 +45,9 @@ public class Optional_03_Test {
         Optional<Account> accOpt = Optional.ofNullable(account);
         // TODO A l'aide de la méthode map récupérer le prénom (account -> person -> firstname)
         // TODO Utiliser la méthode orElseThrow pour déclencher l'exception GoodException si non trouvé
-        // accOpt.map...
+        accOpt.map(a -> a.getOwner())
+	        .map(a -> a.getFirstname())
+	        .orElseThrow(() -> new GoodException());
     }
 
     @Test(expected = GoodException.class)
@@ -54,7 +56,10 @@ public class Optional_03_Test {
         Optional<Account> accOpt = Optional.ofNullable(account);
         // TODO A l'aide de la méthode map récupérer le prénom (account -> person -> firstname)
         // TODO Utiliser la méthode orElseThrow pour déclencher l'exception GoodException si non trouvé
-        // accOpt.map...
+        accOpt.map(a -> a.getOwner())
+	        .map(a -> a.getFirstname())
+	        .orElseThrow(() -> new GoodException());
+
     }
 
     @Test(expected = GoodException.class)
@@ -63,7 +68,9 @@ public class Optional_03_Test {
         Optional<Account> accOpt = Optional.ofNullable(account);
         // TODO A l'aide de la méthode map récupérer le prénom (account -> person -> firstname)
         // TODO Utiliser la méthode orElseThrow pour déclencher l'exception GoodException si non trouvé
-        // accOpt.map...
+        accOpt.map(a -> a.getOwner())
+        	.map(a -> a.getFirstname())
+        	.orElseThrow(() -> new GoodException());
     }
 
     @Test
@@ -72,6 +79,8 @@ public class Optional_03_Test {
         Optional<Account> accOpt = Optional.ofNullable(account);
         // TODO A l'aide de la méthode map récupérer le prénom (account -> person -> firstname)
         // TODO Utiliser la méthode ifPresent pour valider que le prénom est "A"
-        // accOpt.map...
+        accOpt.map(a -> a.getOwner())
+        	.map(a -> a.getFirstname())
+        	.orElseThrow(() -> new GoodException());
     }
 }
